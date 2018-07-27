fd2d616724e80ef898c1566fa5cf00e5dcffa9d2
Gitanjali Nambiar C <gitunambiar@gmail.com>
2018-07-28 02:34:21 +0530

    Added submissions

---
+++ HackerRank/30DaysOfCode/Day8.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            Map<String,int> myMap = new HashMap<String,int>();
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here 
+        }
+        in.close();
+    }
+}
